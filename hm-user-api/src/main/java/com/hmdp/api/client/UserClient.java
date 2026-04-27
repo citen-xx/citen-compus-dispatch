28de1455d763455e85a9e0642f8ac63a5d52435b
Yihua Liu <2659625710@qq.com>
2026-04-27 08:15:46 +0800

    Integrate gateway and Feign user service

--- hm-user-api/src/main/java/com/hmdp/api/client/UserClient.java
+++ hm-user-api/src/main/java/com/hmdp/api/client/UserClient.java
@@ -1,10 +1,19 @@
 package com.hmdp.api.client;
 
+import com.hmdp.dto.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
- * 用户服务 Feign 客户端接口，后续在此补充远程调用方法。
+ * 用户服务 Feign 客户端。
  */
-@FeignClient(name = "hm-user-service", path = "/users")
+@FeignClient("user-service")
 public interface UserClient {
+
+    /**
+     * 根据用户 ID 查询用户信息，对应 UserController 中的查询接口。
+     */
+    @GetMapping("/user/{id}")
+    Result queryUserById(@PathVariable("id") Long userId);
 }
